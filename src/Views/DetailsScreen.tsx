8fcc5b3ad8e25363b5a0d79dd246530bda7d7d6a
IzuEneh <izueneh21@gmail.com>
2023-03-06 21:39:49 -0700

    Start redesign of details page

--- src/Views/DetailsScreen.tsx
+++ src/Views/DetailsScreen.tsx
@@ -1,16 +1,21 @@
 import React, { useState } from "react";
 import {
-	View,
-	StyleSheet,
-	Image,
-	Text,
-	Pressable,
+	View as RNView,
+	Pressable as RNPressable,
+	Text as RNText,
+	ImageBackground as RNImageBackground,
 	Linking,
 	Alert,
 } from "react-native";
 import type { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { styled } from "nativewind";
+
 import { Business, RootStackParamList } from "../../App";
 
+const View = styled(RNView);
+const ImageBackground = styled(RNImageBackground);
+const Text = styled(RNText);
+const Pressable = styled(RNPressable);
 type NavProp = NativeStackScreenProps<RootStackParamList, "Details">;
 
 function DetailsScreen({ route }: NavProp) {
@@ -18,98 +23,37 @@ function DetailsScreen({ route }: NavProp) {
 	const [index, setIndex] = useState(0);
 	const [business, setBusiness] = useState(businesses[index]);
 	return (
-		<View style={styles.container}>
-			<Image
-				style={styles.image}
+		<View className="flex-1 items-center justify-center">
+			<ImageBackground
+				className="w-full basis-2/3"
+				resizeMode="contain"
 				source={{
 					uri: business.image_url,
 				}}
 			/>
-			<Text>{business.name}</Text>
-			<Text>Location: {business.location.address1}</Text>
-			<Text>Rating: {business.rating}</Text>
-			<View style={{ alignItems: "center" }}>
-				<Text>Category</Text>
-				<View style={styles.categories}>
-					{business.categories.map((cat, index) => (
-						<Text key={index}>{cat.title}</Text>
-					))}
+			<View className="w-full basis-1/3 pb-10 px-6 pt-3">
+				<View className="basis-2/3 w-full">
+					<Text>{business.name}</Text>
+					<Text>
+						{business.categories.map((cat, index) => (
+							<Text key={index}>{cat.title} </Text>
+						))}
+					</Text>
+					<Text>{business.rating} / 5</Text>
 				</View>
-			</View>
-			<View style={styles.buttonContainer}>
-				<Pressable
-					style={[
-						styles.button,
-						{
-							backgroundColor: "lightblue",
-							display: index > 0 ? "flex" : "none",
-						},
-					]}
-					onPress={() => {
-						// new stuff
-						const newIndex = index - 1;
-						setIndex(newIndex);
-						setBusiness(businesses[newIndex]);
-					}}
-				>
-					<Text>Previous</Text>
-				</Pressable>
-				<Pressable
-					style={[styles.button, { backgroundColor: "orange" }]}
-					onPress={() => goToMaps(business.coordinates)}
-				>
-					<Text>Go</Text>
-				</Pressable>
-				<Pressable
-					style={[
-						styles.button,
-						{
-							backgroundColor: "red",
-							display: index < businesses.length - 1 ? "flex" : "none",
-						},
-					]}
-					onPress={() => {
-						// console.log("pressed " + index);
-						const newIndex = index + 1;
-						setIndex(newIndex);
-						setBusiness(businesses[newIndex]);
-					}}
-				>
-					<Text>Next</Text>
-				</Pressable>
+				<View className="w-full basis-1/3">
+					<Pressable
+						onPress={() => goToMaps(business.coordinates)}
+						className="bg-sky-400 active:bg-sky-500 w-full h-10 rounded-md justify-center items-center"
+					>
+						<Text className="text-lg font-medium text-white">GO</Text>
+					</Pressable>
+				</View>
 			</View>
 		</View>
 	);
 }
 
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		justifyContent: "center",
-		alignItems: "center",
-		gap: 15,
-	},
-	image: {
-		height: 200,
-		width: 200,
-	},
-	button: {
-		height: 25,
-		width: 100,
-		borderRadius: 10,
-		alignItems: "center",
-		justifyContent: "center",
-	},
-	buttonContainer: {
-		flexDirection: "row",
-		justifyContent: "center",
-		gap: 10,
-	},
-	categories: {
-		flexDirection: "row",
-		gap: 10,
-	},
-});
 
 const goToMaps = async ({ latitude, longitude }: Business["coordinates"]) => {
 	const url = `https://www.google.com/maps/dir/?api=1&destination=${latitude},${longitude}`;
